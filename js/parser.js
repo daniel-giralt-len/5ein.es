1b4a4561a7ea53517abfbc61d4d7fcd9d1202cf4
daniel-giralt-len <daniel.giralt.len@gmail.com>
2021-07-27 15:27:25 +0200

    translate "àcid"

--- js/parser.js
+++ js/parser.js
@@ -3107,7 +3107,7 @@ Parser.ITEM_TYPE_JSON_TO_ABV = {
 };
 
 Parser.DMGTYPE_JSON_TO_FULL = {
-	"A": "acid",
+	"A": "àcid",
 	"B": "bludgeoning",
 	"C": "cold",
 	"F": "fire",
@@ -3122,7 +3122,7 @@ Parser.DMGTYPE_JSON_TO_FULL = {
 	"T": "thunder",
 };
 
-Parser.DMG_TYPES = ["acid", "bludgeoning", "cold", "fire", "force", "lightning", "necrotic", "piercing", "verí", "psíquic", "radiant", "slashing", "thunder"];
+Parser.DMG_TYPES = ["àcid", "bludgeoning", "cold", "fire", "force", "lightning", "necrotic", "piercing", "verí", "psíquic", "radiant", "slashing", "thunder"];
 Parser.CONDITIONS = ["encegat", "fascinat", "eixordat", "exhaust", "acovardat", "grappled", "incapacitated", "invisible", "paralitzat", "petrificat", "emmetzinat", "prone", "restrained", "stunned", "unconscious"];
 
 Parser.SKILL_JSON_TO_FULL = {
