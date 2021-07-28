405d67bd7c7d2de51ebe964f0e6d2dad2e2b3acf
daniel-giralt-len <daniel.giralt.len@gmail.com>
2021-07-28 13:25:15 +0200

    or half as much damage on a successful one.

--- js/converter.js
+++ js/converter.js
@@ -349,7 +349,7 @@ CreatureConverter.SAMPLE_MARKDOWN =
 >- **Cantrip.** The lich casts a cantrip.
 >- **Paralyzing Touch (Costs 2 Actions).** The lich uses its Paralyzing Touch.
 >- **Frightening Gaze (Costs 2 Actions).** The lich fixes its gaze on one creature it can see within 10 feet of it. The target must succeed on a DC 18 Wisdom saving throw against this magic or become frightened for 1 minute. The frightened target can repeat the saving throw at the end of each of its turns, ending the effect on itself on a success. If a target's saving throw is successful or the effect ends for it, the target is immune to the lich's gaze for the next 24 hours.
->- **Disrupt Life (Costs 3 Actions).** Each non-undead creature within 20 feet of the lich must make a DC 18 Constitution saving throw against this magic, taking 21 (6d6) necrotic damage on a failed save, or half as much damage on a successful one.
+>- **Disrupt Life (Costs 3 Actions).** Each non-undead creature within 20 feet of the lich must make a DC 18 Constitution saving throw against this magic, taking 21 (6d6) necrotic damage on a failed save, ; si supera, en rep la meitat.
 >
 >`;
 // endregion
