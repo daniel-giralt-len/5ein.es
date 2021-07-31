e60c69ccca88fb972829ae4a09f2d57ce2d084f8
daniel-giralt-len <daniel.giralt.len@gmail.com>
2021-07-31 11:56:55 +0200

    "Tiefling"

--- js/lifegen.js
+++ js/lifegen.js
@@ -179,7 +179,7 @@ function rollOnArray (lst) {
 	return lst[RNG(lst.length) - 1]
 }
 
-const RACES_SELECTABLE = ["Dwarf", "Elf", "Semi-Elf", "Semi-Orc", "Tiefling"];
+const RACES_SELECTABLE = ["Dwarf", "Elf", "Semi-Elf", "Semi-Orc", "Tíflin"];
 const RACES_UNSELECTABLE = ["Humà", "Halfling", "Dragonborn", "Gnom"];
 
 const PARENTS_HALF_ELF = [
@@ -198,8 +198,8 @@ const PARENTS_HALF_ORC = [
 
 const PARENTS_TIEFLING = [
 	{min: 1, max: 4, display: "Both parents were humans, their infernal heritage dormant until you came along.", _races: ["Humà", "Humà"]},
-	{min: 5, max: 6, result: () => { const p = RNG(2); return `One parent ${fmtChoice(p === 1 ? "mother" : "father")} was a tiefling and the other ${fmtChoice(p === 1 ? "father" : "mother")} was a human.` }, display: "One parent was a tiefling and the other was a human.", _races: ["Humà", "Tiefling"]},
-	{min: 7, result: () => { const p = RNG(2); return `One parent ${fmtChoice(p === 1 ? "mother" : "father")} was a tiefling and the other ${fmtChoice(p === 1 ? "father" : "mother")} was a devil.` }, display: "One parent was a tiefling and the other was a devil.", _races: ["Devil", "Tiefling"]},
+	{min: 5, max: 6, result: () => { const p = RNG(2); return `One parent ${fmtChoice(p === 1 ? "mother" : "father")} was a tiefling and the other ${fmtChoice(p === 1 ? "father" : "mother")} was a human.` }, display: "One parent was a tiefling and the other was a human.", _races: ["Humà", "Tíflin"]},
+	{min: 7, result: () => { const p = RNG(2); return `One parent ${fmtChoice(p === 1 ? "mother" : "father")} was a tiefling and the other ${fmtChoice(p === 1 ? "father" : "mother")} was a devil.` }, display: "One parent was a tiefling and the other was a devil.", _races: ["Devil", "Tíflin"]},
 	{min: 8, result: () => { const p = RNG(2); return `One parent ${fmtChoice(p === 1 ? "mother" : "father")} was a human and the other ${fmtChoice(p === 1 ? "father" : "mother")} was a devil.` }, display: "One parent was a human and the other was a devil.", _races: ["Humà", "Devil"]},
 ];
 
@@ -530,7 +530,7 @@ const SUPP_RACE = [
 	{min: 76, max: 80, result: "Gnom"},
 	{min: 81, max: 85, result: "Semi-elf"},
 	{min: 86, max: 90, result: "Semi-orc"},
-	{min: 91, max: 95, result: "Tiefling"},
+	{min: 91, max: 95, result: "Tíflin"},
 	{min: 96, max: 100, result: "DM’s choice"},
 ];
 
@@ -609,7 +609,7 @@ function onJsonLoad (lifeData, nameData) {
 				else nameTables["halfelf"] = cpy;
 			} else if (nameMeta.name === "Semi-Orc") {
 				nameTables["orc"] = MiscUtil.copy(nameMeta);
-			} else if (nameMeta.name === "Tiefling") {
+			} else if (nameMeta.name === "Tíflin") {
 				const cpy = MiscUtil.copy(nameMeta);
 				cpy.tables = cpy.tables.filter(it => it.option !== "Virtue");
 				nameTables["devil"] = MiscUtil.copy(nameMeta);
@@ -681,7 +681,7 @@ function sectParents () {
 				parentRaces = rolled._races;
 				break;
 			}
-			case "tiefling": {
+			case "tíflin": {
 				const rolled = GenUtil.getFromTable(PARENTS_TIEFLING, RNG(8));
 				parentage = `<b>${race} parents:</b> ${rolled.result}`;
 				parentRaces = rolled._races;
