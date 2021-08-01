e72cb16b1206fc571e3c217cfc85893aad720b57
daniel-giralt-len <daniel.giralt.len@gmail.com>
2021-08-01 19:18:41 +0200

    you can see within range

--- js/converter.js
+++ js/converter.js
@@ -405,7 +405,7 @@ Casting Time: 1 action
 Range: 90 feet
 Components: V, S, M (a diamond worth at least 50 gp)
 Duration: Instantani
-You hurl a 4-inch-diameter sphere of energy at a creature that you can see within range. You choose acid, cold, fire, lightning, poison, or thunder for the type of orb you create, and then make a ranged spell attack against the target. If the attack hits, the creature takes 3d8 damage of the type you chose.
+You hurl a 4-inch-diameter sphere of energy at a creature that que puguis veure a l'abast. You choose acid, cold, fire, lightning, poison, or thunder for the type of orb you create, and then make a ranged spell attack against the target. If the attack hits, the creature takes 3d8 damage of the type you chose.
 A Nivells Superiors. Quan llances aquest conjur emprant una ranura de nivell 2 o superior, el dany incrementa en 1d8 per cada nivell per sobre d'1.`;
 // endregion
 
