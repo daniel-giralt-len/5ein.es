4c7de22e7bbeca4023de86a93b49cdb37e3e3048
daniel-giralt-len <daniel.giralt.len@gmail.com>
2021-09-08 16:32:25 +0200

    all Poison Spray

--- js/scalecreature.js
+++ js/scalecreature.js
@@ -233,7 +233,7 @@
 	},
 
 	// cantrips that should be preserved when lowering the number of cantrips known, to ensure caster effectiveness
-	_protectedCantrips: ["esquitx àcid", "mà morta", "missil èldritx", "sageta de foc", "poison spray", "produce flame", "raig gebre", "sacred flame", "shocking grasp", "thorn whip", "vicious mockery"],
+	_protectedCantrips: ["esquitx àcid", "mà morta", "missil èldritx", "sageta de foc", "esprai de verí", "produce flame", "raig gebre", "sacred flame", "shocking grasp", "thorn whip", "vicious mockery"],
 
 	// analysis of official data + some manual smoothing
 	_crToCasterLevelAvg: {
