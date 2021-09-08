eba083056e944b69894aaa121c1768cdc75c050d
daniel-giralt-len <daniel.giralt.len@gmail.com>
2021-09-08 19:40:08 +0200

    all Scrying

--- js/render.js
+++ js/render.js
@@ -4649,7 +4649,7 @@ Renderer.monster = {
 				},
 				6: {
 					W: ["cone of cold", "wall of ice"],
-					A: ["scrying", "Vincle Telepàtic d'en Rary", "Dansa Irresistible de l'Otto", "recordar llegendes", "immobilitzar criatura", "somnis"],
+					A: ["escodrinyar", "Vincle Telepàtic d'en Rary", "Dansa Irresistible de l'Otto", "recordar llegendes", "immobilitzar criatura", "somnis"],
 				},
 				7: {
 					B: ["power word pain|XGE", "dit de la mort", "desintegrar", "immobilitzar criatura"],
