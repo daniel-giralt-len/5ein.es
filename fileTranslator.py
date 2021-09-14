eb936332b729b6753f1507666a8f720ffbbe93cd
daniel-giralt-len <daniel.giralt.len@gmail.com>
2021-09-14 20:38:11 +0200

    first approach to translating longer strings

--- fileTranslator.py
+++ fileTranslator.py
@@ -2,13 +2,20 @@ from deep_translator import GoogleTranslator
 import json
 import sys
 
+google_translator = GoogleTranslator(source='english', target='catalan')
+TRANSLATOR_MAX_STRING_LENGTH = 5000
+
+def translate_string(string):
+	string_parts = [google_translator.translate(string[0+i:TRANSLATOR_MAX_STRING_LENGTH+i]) for i in range(0, len(string), TRANSLATOR_MAX_STRING_LENGTH)]
+	return ''.join(string_parts)
+
 def translate_object(object):
 	for key, value in object.items():
 		value_type = type(value)
 		if value_type is str:
 			print(f'translated {key}')
 
-			translated_string = GoogleTranslator(source='english', target='catalan').translate(value)
+			translated_string = translate_string(value)
 			object[key] = translated_string
 		elif value_type is list:
 			print(f'{key} is list')
@@ -19,7 +26,7 @@ def translate_object(object):
 				list_entry_type = type(list_entry)
 
 				if list_entry_type is str:
-					translated_list_entry = GoogleTranslator(source='english', target='catalan').translate(list_entry)
+					translated_list_entry = translate_string(list_entry)
 					print(f'translating {translated_list_entry}')
 				elif list_entry_type is dict:
 					translated_list_entry = translate_object(list_entry)
