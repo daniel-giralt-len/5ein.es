ef958595df05c2f0226bf3f7c383d99ba4deb6eb
daniel-giralt-len <daniel.giralt.len@gmail.com>
2021-09-14 15:07:42 +0200

    improve fileTranslator readability

--- fileTranslator.py
+++ fileTranslator.py
@@ -1,34 +1,38 @@
 from deep_translator import GoogleTranslator
 import json
+import sys
 
-FILE_PATH = "./data/class/class-mystic.json"
+def translate_object(object):
+	for key, value in object.items():
+		value_type = type(value)
+		if value_type is str:
+			print(f'translated {key}')
 
-def translate_object(o):
-	for k,v in o.items():
-		tv = type(v)
-		print(f'{k}, {tv}')
-		if tv is str:
-			nv = GoogleTranslator(source='english', target='catalan').translate(v)
-			print(f'translated to {nv}')
-			o[k] = nv
-		elif tv is list:
-			nl = []
-			print('is list')
-			for av in v:
-				nav = av
-				if type(av) is str:
-					nav = GoogleTranslator(source='english', target='catalan').translate(av)
-					print(f'translating {v} to {nav}')
-				elif type(av) is dict:
-					nav = translate_object(av)
-				nl.append(nav)
-			print(nl)
-			o[k] = nl
-		elif tv is dict:
-			translate_object(v)
-	return o
+			translated_string = GoogleTranslator(source='english', target='catalan').translate(value)
+			object[key] = translated_string
+		elif value_type is list:
+			print(f'{key} is list')
 
-with open(FILE_PATH, 'r', encoding="utf-8") as f:
-	j = translate_object(json.load(f))
-	with open(FILE_PATH, 'w', encoding="utf-8") as w:
-		w.write(json.dumps(j, ensure_ascii=False, indent='\t'))
+			translated_list = []
+			for list_entry in value:
+				translated_list_entry = list_entry
+				list_entry_type = type(list_entry)
+
+				if list_entry_type is str:
+					translated_list_entry = GoogleTranslator(source='english', target='catalan').translate(list_entry)
+					print(f'translating {translated_list_entry}')
+				elif list_entry_type is dict:
+					translated_list_entry = translate_object(list_entry)
+
+				translated_list.append(translated_list_entry)
+			
+			print(translated_list)
+			object[key] = translated_list
+		elif value_type is dict:
+			translate_object(value)
+	return object
+
+with open(sys.argv[1], 'r', encoding="utf-8") as input_file_data:
+	translated_json = translate_object(json.load(input_file_data))
+	with open(sys.argv[1], 'w', encoding="utf-8") as w:
+		w.write(json.dumps(translated_json, ensure_ascii=False, indent='\t'))
