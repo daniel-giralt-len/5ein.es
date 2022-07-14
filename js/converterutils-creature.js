eea20fcfcc1699e59e067a65aaa6cf873ce250eb
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-07-14 10:11:23 +0200

    Elven Chain

--- js/converterutils-creature.js
+++ js/converterutils-creature.js
@@ -114,7 +114,7 @@ class AcConvert {
 
 						// magic items
 						case "plaques nanes": froms.push("{@item plaques nanes}"); break;
-						case "elven chain": froms.push("{@item elven chain}"); break;
+						case "malla èlfica": froms.push("{@item malla èlfica}"); break;
 						case "glamoured studded leather": froms.push("{@item glamoured studded leather}"); break;
 						case "braçals de defensa": froms.push("{@item braçals de defensa}"); break;
 						case "badge of the watch": froms.push("{@item Badge of the Watch|wdh}"); break;
